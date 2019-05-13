a065d332430c3dba6cf7dffce6d3393c2fe860a8
OlliKley <ok@wildnissport.de>
2019-05-13 16:36:36 +0200

    fixed wrong filename

--- wooCommerce/c-novation-pay-payments-woo-commerce/includes/CNovationPayClient.php
+++ wooCommerce/c-novation-pay-payments-woo-commerce/includes/CNovationPayClient.php
@@ -2,66 +2,56 @@
 
 class CNovationPayClient
 {
-	private $url = "https://coinworxx.scavix.net/api";
-	private $sandbox_url = "https://coinworxx.scavix.net/api";
-	var $token = false;
-	var $secret = false;
+    var $url = "https://www.c-novation-pay.com/api";
+    var $token = false;
+    var $secret = false;
     var $response = false;
     var $success = false;
     var $result = false;
     var $error = false;
 
-    protected $isSandbox = false;
-	
-	public function __construct($api_end_point, $token=false, $secret=false, $isSandbox = false)
-	{
-		$this->url = $api_end_point;
-		$this->sandbox_url = $api_end_point;
-		$this->token = $token;
-		$this->secret = $secret;
-		$this->isSandbox = $isSandbox;
-	}
+    public function __construct($token=false, $secret=false)
+    {
+        $this->token = $token;
+        $this->secret = $secret;
+    }
 
-	
-	protected function callApi($path,$arguments=array())
-	{
+    protected function callApi($path,$arguments=array())
+    {
         $this->response = $this->success = $this->result = $this->error = false;
-         $url = $this->isSandbox?$this->sandbox_url:$this->url;
-        
-		$url .= "/{$this->token}/{$path}";
-        
+        $url = "{$this->url}/{$this->token}/{$path}";
+
         $arguments = array_filter($arguments,function($a){ return !is_null($a); });
-       
+
         if( $this->secret )
             $arguments['secret'] = $this->secret;
-        
-		if( count($arguments)>0 )
-			$url .= "?".http_build_query($arguments);
-		
-		$ch = curl_init();
-		CNovationPayGateway::log(__FILE__ . ' . ' . __LINE__ . ': '. $url);
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+        if( count($arguments)>0 )
+            $url .= "?".http_build_query($arguments);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 
 		/**
 		 * for local dev only
 		 */
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		
+//		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
 		$this->response = curl_exec($ch);
 		$error = curl_error($ch);
 		curl_close($ch);
-		
+
 		if( !$this->response )
 			throw new CNovationException("CURL: {$error}");
-		
+
 		$json = json_decode($this->response,true);
 		if( !$json )
 			throw new CNovationException("INVALID JSON: {$this->response}");
-            
+
         $this->success = $json['success'];
-        
+
         if( !$this->success )
         {
             if( !isset($json['code']) || !$json['code'] )
@@ -74,11 +64,11 @@ class CNovationPayClient
             return false;
         }
         $this->result = $json['result'];
-		return $this->result;
-	}
-	
-	public function info() { return $this->callApi(""); }
-    
+        return $this->result;
+    }
+
+    public function info() { return $this->callApi(""); }
+
     public function register($identifier,$type='')
     {
         if( !$this->secret )
@@ -89,33 +79,32 @@ class CNovationPayClient
         }
         return $this->secret;
     }
-	
-	public function currencies() { return $this->callApi("currencies"); }
-	public function currency($code) { return $this->callApi("currencies/{$code}"); }
 
-	public function wallets() { return $this->callApi("wallets"); }
-	public function wallet($uid) { return $this->callApi("wallets/{$uid}"); }
-	
-	public function payments() { return $this->callApi("payments"); }
-	public function payment($uid) { return $this->callApi("payments/{$uid}"); }
-	public function createPayment($currency, $price, $price_currency, $reference, $deadline=null, $callback=null) 
+    public function currencies() { return $this->callApi("currencies"); }
+    public function currency($code) { return $this->callApi("currencies/{$code}"); }
+
+    public function wallets() { return $this->callApi("wallets"); }
+    public function wallet($uid) { return $this->callApi("wallets/{$uid}"); }
+
+    public function payments() { return $this->callApi("payments"); }
+    public function payment($uid) { return $this->callApi("payments/{$uid}"); }
+    public function createPayment($currency, $price, $price_currency, $reference, $deadline=null, $callback=null)
     {
         return $this->callApi(
             "payments/create",
             compact('currency','price','price_currency','reference','deadline','callback')
         );
     }
-	public function cancelPayment($uid) { return $this->callApi("payments/cancel/{$uid}"); }
+    public function cancelPayment($uid) { return $this->callApi("payments/cancel/{$uid}"); }
 
-	public function checkout($ident, $price, $price_currency, $reference, $callback, $return_url, $cancel_url = '')
-	{
-		return $this->callApi(
-			"payments/checkout",
-			compact('ident', 'price', 'price_currency', 'reference', 'callback', 'return_url', 'cancel_url')
-		);
+    public function checkout($ident, $price, $price_currency, $reference, $callback, $return_url, $cancel_url = '')
+    {
+        return $this->callApi(
+            "payments/checkout",
+            compact('ident', 'price', 'price_currency', 'reference', 'callback', 'return_url', 'cancel_url')
+        );
 
-	}
-
+    }
 }
 
 class CNovationException extends \Exception { }
